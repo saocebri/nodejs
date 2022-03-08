cf3808f8477ca3cbff33063366400d939c545a06
saocebri <albacarlos2012@gmail.com>
2022-03-07 23:01:18 -0300

    Meu primeiro Hello World em Nodejs

--- projetos-express/src/index.js
+++ projetos-express/src/index.js
@@ -6,8 +6,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World NodeJS! Componente padrão');
 });
+
+app.get('/uma-lista',(req, resp)=>{
+res.send('Esse é o conteudo para uma pagina componente(uma lista)');
+});
+
+app.get('/zf*zy', (res, req)=>{
+    res.send('Novo componente acessado a partir de rota customizada');
+})
 
 //vamos implementar o escutador da requisição
 
